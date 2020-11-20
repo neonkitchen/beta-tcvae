2cb485ac479bb3984a6701a54e245743653e8abe
neelan pather <neelan.pather@gmail.com>
2020-11-20 12:20:15 +0200

    batchsize 8

--- vae_quant.py
+++ vae_quant.py
@@ -367,7 +367,7 @@ def main():
         choices=['shapes', 'faces'])
     parser.add_argument('-dist', default='normal', type=str, choices=['normal', 'laplace', 'flow'])
     parser.add_argument('-n', '--num-epochs', default=50, type=int, help='number of training epochs')
-    parser.add_argument('-b', '--batch-size', default=2048/4, type=int, help='batch size')
+    parser.add_argument('-b', '--batch-size', default=8, type=int, help='batch size')
     parser.add_argument('-l', '--learning-rate', default=1e-3, type=float, help='learning rate')
     parser.add_argument('-z', '--latent-dim', default=10, type=int, help='size of latent dimension')
     parser.add_argument('--beta', default=1, type=float, help='ELBO penalty term')
